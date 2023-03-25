7021c6d46a8cdbb95817c066e4367f2b236b4adb
goohayami <goohayami@gmail.com>
2023-03-25 23:46:02 +0900

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://goohayami.github.io",
   base: "/astroTest",
+  integrations: [vue()],
 });
